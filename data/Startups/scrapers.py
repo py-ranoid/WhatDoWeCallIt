5d136c633a12b5c37234bac612c6b577b2c3f4dd
Vishal Gupta <vishalg8897@gmail.com>
2018-04-01 09:24:02 +0530

    Cleansed scrapers.py

--- data/Startups/scrapers.py
+++ data/Startups/scrapers.py
@@ -1,32 +1,37 @@
 from bs4 import BeautifulSoup as soup
 from urllib2 import urlopen
-url = "http://yclist.com/"
-s = soup(urlopen(url).read(), 'lxml')
-yc = []
-for i in s.find('table').find_all('tr')[1:]:
-    try:
-        yc.append(str(i.find_all('td')[1].text))
-    except:
-        pass
-
 import pandas as pd
-ind = list(pd.read_csv('data/Startups/startup_funding.csv')
-           ['StartupName'].astype('str'))
 
 
-all_names = set(ind).union(set(yc))
-len(all_names)
+def get_yc():
+    url = "http://yclist.com/"
+    s = soup(urlopen(url).read(), 'lxml')
+    yc = []
+    for i in s.find('table').find_all('tr')[1:]:
+        try:
+            yc.append(str(i.find_all('td')[1].text))
+        except UnicodeError:
+            pass
+    return yc
 
 
-def fetch():
-    return all_names
-# import requests
-#
-# response = requests.get("https://community-angellist.p.mashape.com/startups?filter=raising&access_token=kB0vTc8gJ8mshnYZg8Z2Gv2qBJmlp17oaZKjsngZ82UBIjXePv",
-#                         headers={
-#                             "X-Mashape-Key": "kB0vTc8gJ8mshnYZg8Z2Gv2qBJmlp17oaZKjsngZ82UBIjXePv",
-#                             "Accept": "text/plain"
-#                         }
-#                         )
-#
-# response.content
+def get_ind():
+    df = pd.read_csv('data/Startups/startup_funding.csv')
+    df.head()
+    ind = list(df['StartupName'].astype('str'))
+    return ind
+
+
+def get_big():
+    df = pd.read_csv('data/Startups/startups.csv')
+    df.head()
+    big = list(df.dropna()['name'])
+    return big
+
+
+def get_startups():
+    funcs = [get_yc, get_ind, get_big]
+    all = set()
+    for f in funcs:
+        all = all.union(set(f()))
+    return list(all)
