a622e7449ffaf9e5cf0023683d0206bbf8d0ec87
Vishal Gupta <vishalg8897@gmail.com>
2018-04-03 20:06:51 +0530

    Added models and script to test.

--- LSTMGen/KerasGen.py
+++ LSTMGen/KerasGen.py
@@ -141,10 +141,10 @@ print("Saved model to disk")
 
 
 def load():
-    json_file = open('model.json', 'r')
+    json_file = open('model2.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
-    loaded_model.load_weights("model.h5")
+    loaded_model.load_weights("model2.h5")
     print("Loaded model from disk")
